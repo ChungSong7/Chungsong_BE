b310bf7000c4df20a5b2d692a746e130614ac3f5
(yeseul) <(choiyeseul@g.hongik.ac.kr)>
2024-01-31 13:31:39 +0900

    [feat] 회원탈퇴API 추가

--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import SignupView,LoginView,UserInfoView,LogoutView,RefreshTokenView
+from .views import SignupView,LoginView,UserInfoView,LogoutView,RefreshJWTTokenView
 
 urlpatterns = [
     path('signup/', SignupView.as_view(),name='SignUp'),
     path('login/',LoginView.as_view(),name='Login'),
-    path('info/',UserInfoView.as_view(),name='UserInfo'),
-    path('refresh_token/',RefreshTokenView.as_view(),name='RefreshToken'),
+    path('user_info/',UserInfoView.as_view(),name='UserInfo'),
+    path('refresh_jwt_token/',RefreshJWTTokenView.as_view(),name='RefreshJWTToken'),
     path('logout/',LogoutView.as_view(),name='Logout'),
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
--- users/views.py
+++ users/views.py
@@ -1,10 +1,14 @@
 from rest_framework.views import APIView
-from rest_framework.authentication import get_authorization_header
+from rest_framework.authentication import get_authorization_header,TokenAuthentication
 from rest_framework.response import Response
+from rest_framework import status
 from rest_framework.exceptions import AuthenticationFailed
+from rest_framework.permissions import IsAuthenticated
 from .serializers import UserSerializer
 from .models import User
 from .authentications import create_access_token,create_refresh_token,decode_access_token,decode_refresh_token
+
+#from django.contrib.auth import logout
 
 #회원가입 API
 class SignupView(APIView):
@@ -53,8 +57,11 @@ class LoginView(APIView):
         #serializer=UserSerializer(user)
         #return Response(serializer.data)
         
-    
+
+#유저정보 조회,삭제 API
 class UserInfoView(APIView):
+
+    #유저정보 조회
     def get(self,request):
         auth=get_authorization_header(request).split()
         if auth and len(auth)==2: #auth[0]=='Bearer'
@@ -65,8 +72,37 @@ class UserInfoView(APIView):
             return Response(serializer.data)
         
         raise AuthenticationFailed('unauthenticated')
+    
+    #회원탈퇴
+    def delete(self,request):
+        #너가 누군지 jwt로 찾을게
+        auth=get_authorization_header(request).split()
+        if auth and len(auth)==2: #auth[0]=='Bearer'
+            token=auth[1].decode('utf-8') #토큰 추출
+            user_id=decode_access_token(token) #토큰에서 유저 고유번호 추출
+            try:
+                user = User.objects.get(user_id=user_id)
+            except User.DoesNotExist:
+                raise AuthenticationFailed('User not found')
+            
+            password = request.data.get('password', None)
+            if not password:
+                return Response({'error': 'please input password'}, status=status.HTTP_400_BAD_REQUEST)
+            if not user.check_password(password): #비밀번호 올바르게 입력했니?
+                raise AuthenticationFailed('Incorrect password')
+            
+            #계정삭제
+            user.delete()
+            #로그아웃
+            response = Response({'message': '회원탈퇴가 성공적으로 처리되었습니다.'}, status=status.HTTP_204_NO_CONTENT)
+            response.delete_cookie('refresh_token')
+            return response
+        raise AuthenticationFailed('unauthenticated')
 
-class RefreshTokenView(APIView):
+    
+
+#access_token 재발급 API
+class RefreshJWTTokenView(APIView):
     def post(self, request):
         refresh_token=request.COOKIES.get('refresh_token')
         user_id=decode_refresh_token(refresh_token)
@@ -76,11 +112,10 @@ class RefreshTokenView(APIView):
             'access_token':access_token
             })
 
+#로그아웃 API
 class LogoutView(APIView):
     def post(self, request):
-        response=Response()
+        response=Response({'message':'logout success'})
         response.delete_cookie(key='refresh_token')
-        response.data={
-            'message':'logout success'
-        }
         return response
+
