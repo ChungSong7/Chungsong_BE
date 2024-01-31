fdb5d9e80e4bf6af05703bff2282ba94a6053395
soyeoneeii <syp0463@gmail.com>
2024-01-31 19:02:59 +0900

    [feat]posts model 작성

--- config/urls.py
+++ config/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 
+from django.conf.urls.static import static
+from django.conf import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     #path('users/',include('users.urls')),
@@ -24,3 +27,6 @@ urlpatterns = [
     #path('comments/',include('comments.urls')),
     #path('complains/',include('complains.urls')),
 ]
+#이미지 경로 추가
+#/media/이미지이름.png 이렇게 저장됨
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # media 경로 추가
--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+#게시판
+class Board(models.Model):
+    board_id = models.IntegerField(verbose_name="게시판ID", primary_key=True, unique=True, editable=False)
+    board_name = models.CharField(verbose_name="게시판이름", max_length = 10)
+
+#게시글
+class Post(models.Model):
+    post_id = models.UUIDField(verbose_name='게시글ID', primary_key=True, default=uuid.uuid4, unique=True, editable=False)
+    board = models.ForeignKey(Board, on_delete=models.CASCADE, verbose_name='게시판', related_name='posts')
+    title = models.CharField(verbose_name='글제목',max_length=50)
+    content = models.TextField(verbose_name='글내용')
+    created_at = models.DateTimeField(verbose_name='작성시간',auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='작성자',related_name ='posts')
+    cmter_size = models.IntegerField(verbose_name='댓작성자수',default =0)
+    like_size = models.IntegerField(verbose_name='좋아요수',default=0)
+    warn_size = models.IntegerField(verbose_name='신고수',default=0)
+    anon_status = models.BooleanField(verbose_name='익명여부',default=True)
+
+#이미지
+class Image(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='images')
+    imgfile = models.ImageField(verbose_name='이미지파일',null=True,upload_to="",blank=True)
