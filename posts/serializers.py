710c138695d3724c4fb3f6c70b4b1e3b56770343
soyeoneeii <syp0463@gmail.com>
2024-02-01 18:55:24 +0900

    [test] 포스트 시리얼라이저 약간 수정 및 깃 테스트용

--- posts/serializers.py
+++ posts/serializers.py
@@ -4,21 +4,18 @@ from .models import Post, Image
 
 #게시글 목록 조회 시리얼라이저
 class PostListSerializer(serializers.ModelSerializer):
-    author_name = serializers.SerializerMethodField()
+    #author_name = serializers.CharField(source=author.) #jwt 구현 코드 통해서 바꾸기 거기서 이름 가져오기
     cmter_size = serializers.IntegerField(source='cmter_size')
     like_size = serializers.IntegerField(source='like_size')
 
     class Meta:
         model = Post
-        fields = ['title', 'content', 'like_size', 'cmter_size', 'created_at', 'author_name']
-
-    def get_author_name(self, obj):
-        return obj.author.username if not obj.anon_status else "익명"
+        fields = ['title', 'content', 'like_size', 'cmter_size', 'created_at', 'author_name','anon_status']
     
 #게시글 단일 조회 시리얼라이저
 class PostDetailSerializer(serializers.ModelSerializer):
-    author_name = serializers.SerializerMethodField()
-    author_profile = serializers.SerializerMethodField()
+    #author_name = serializers.SerializerMethodField()
+    #author_profile = serializers.SerializerMethodField() #프로필을 어떻게 처리해야 하는가? 그냥 author.profile 해서 가져와서 뿌려주고 프론트에서 익명이면 가리게?
     cmter_size = serializers.IntegerField(source='cmter_size')
     like_size = serializers.IntegerField(source='like_size')
 
@@ -28,10 +25,6 @@ class PostDetailSerializer(serializers.ModelSerializer):
 
     def get_author_name(self, obj):
         return obj.author.username if not obj.anon_status else "익명"
-
-    def get_author_profile(self, obj):
-        default_profile = 'default_profile.jpg' #디폴트 프로필 경로 - 변경 필요
-        return obj.author.profile_image.url if not obj.anon_status else default_profile
 
 
 #게시글 작성 시리얼라이저
