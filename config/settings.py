e8602a03e59d725c34c9a9c688d80b65e2ad300e
(yeseul) <(choiyeseul@g.hongik.ac.kr)>
2024-02-07 14:23:17 +0900

    [fix] 뭔지 모르겠음 나도 되돌리기

--- config/settings.py
+++ config/settings.py
@@ -24,10 +24,6 @@ MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-#이미지 파일 저장 경로
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 
 
 # Quick-start development settings - unsuitable for production
