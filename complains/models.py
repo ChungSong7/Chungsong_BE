699790c80a206ea58673eccfca7706ccbcfba2d2
soyeoneeii <syp0463@gmail.com>
2024-02-11 23:56:56 +0900

    [refact]complains 모델, 시리얼라이저 수정

--- complains/models.py
+++ complains/models.py
@@ -7,20 +7,27 @@ import uuid
 
 class Complain(models.Model):
     complain_id = models.UUIDField(verbose_name='신고ID', primary_key=True, default=uuid.uuid4, unique=True, editable=False)
-    comp_post = models.ForeignKey(Post, blank= True, null = True)
-    comp_comment = models.ForeignKey(Comment, blank= True, null = True)
+    comp_post = models.ForeignKey(Post, on_delete=models.SET_NULL, blank= True, null = True)
+    comp_comment = models.ForeignKey(Comment, on_delete=models.SET_NULL,blank= True, null = True)
     comp_user = models.ForeignKey(User,verbose_name='신고자', on_delete=models.CASCADE)
     comped_user = models.ForeignKey(User,verbose_name='피신고자', on_delete=models.CASCADE, related_name='complains')
     comp_date = models.DateField('신고날짜',auto_now_add=True)
 
-    CATEGOTY_CHOICES = ('불건전한 내용',
-                '사기',
-                '상업적 광고',
-                '특정인에 대한 비난',
-                '도배'
-    )
-    category = models.CharField(verbose_name='신고사유', max_length=20, choices=CATEGOTY_CHOICES)
+    CATEGORY_CHOICES = [
+        ('불건전한 내용', '불건전한 내용'),
+        ('사기', '사기'),
+        ('상업적 광고', '상업적 광고'),
+        ('특정인에 대한 비난', '특정인에 대한 비난'),
+        ('도배', '도배'),
+        ('기타', '기타')
+    ]
 
-    status = models.IntegerField(verbose_name='신고처리',default = 0)
+    category = models.CharField(verbose_name='신고사유', max_length=20, choices=CATEGORY_CHOICES)
+
+    STATUS_CHOICES = [
+        (0,'요청중'),
+        (1,'처리완료')
+    ]
+    status = models.IntegerField(verbose_name='신고처리상태', choices=STATUS_CHOICES, default = 0)
 
 
--- complains/serializers.py
+++ complains/serializers.py
@@ -2,15 +2,27 @@ from rest_framework import serializers
 from .models import Complain
 
 from users.serializers import UserSerializer
-#from posts.serializers import PostSerializer
-#from comment.serializers import CommentSerializer
+from posts.serializers import PostSerializer
+from comment.serializers import CommentSerializer
+
 
 class ComplainSerializer(serializers.ModelSerializer):
-    #comp_post = PostSerializer(read_only=True)
-    #comp_comment = CommentSerializer(read_only=True)
-    comped_user = UserSerializer(read_only=True)
+    
+    comp_user_id = serializers.IntegerField(source='comp_user.id', read_only=True)
+    comped_user_id = serializers.IntegerField(source='comped_user.id', read_only=True)
+    comp_post_id = serializers.IntegerField(source='comp_post.id', required=False, allow_null=True)
+    comp_comment_id = serializers.IntegerField(source='comp_comment.id', required=False, allow_null=True)
 
     class Meta:
         model = Complain
-        fields = ['complain_id','comp_post','comp_comment','comp_user','comped_user','comp_date','category','status']
+        fields = [
+            'complain_id',
+            'comp_post_id', 
+            'comp_comment_id',  
+            'comp_user_id',
+            'comped_user_id', 
+            'comp_date',
+            'category',
+            'status',
+        ]
 
