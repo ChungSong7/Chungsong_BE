595feeffe5596e8e783eb001dc46a37e7c4b6ea9
(yeseul) <(choiyeseul@g.hongik.ac.kr)>
2024-02-15 09:44:44 +0900

    [refact] User 모델 status 선택지 수정

--- my_settings.py
+++ my_settings.py
@@ -1,3 +1,9 @@
 ACCESS_TOKEN_SECRET_KEY="CJDTHDzjabslxlDOQ"
 REFRESH_TOKEN_SECRET_KEY='cjdthdZJABSLXLdoq'
 ALGORITHM='HS256'
+
+DB_ENGINE='django.db.backends.mysql'
+DB_NAME='CSdatabase'
+DB_USER='ChungSong'
+DB_PASSWORD='tjdrhdgkwkcjdthd2024!'
+DB_HOST='chungsong-db.cxsoug2uac3u.ap-northeast-2.rds.amazonaws.com'
