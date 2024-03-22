22be2aaa4f0337b6ba3840c19763cfadd3ab12ac
(yeseul) <(choiyeseul@g.hongik.ac.kr)>
2024-03-23 01:35:57 +0900

    [fix] 학교이름 길이 제한 늘림 10->50

--- boards/models.py
+++ boards/models.py
@@ -4,7 +4,7 @@ from django.db import models
 #게시판
 class Board(models.Model):
     board_id = models.AutoField(verbose_name="게시판ID", primary_key=True, unique=True, editable=False)
-    board_name = models.CharField(verbose_name="게시판이름", max_length = 10)
+    board_name = models.CharField(verbose_name="게시판이름", max_length = 50)
     
     def __str__(self):
         return self.board_name
